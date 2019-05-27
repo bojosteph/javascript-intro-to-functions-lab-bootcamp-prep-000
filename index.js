04e3a7eeb202da869d9c78fd5684e8523fa8596e
Roberto Samia <bojosteph@gmail.com>
2019-05-27 16:01:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function whisper(string) {
 }
 
 function logShout(string) {  
-<<<<<<< HEAD
   console.log (string.toUpperCase())
 }
 
 function logWhisper(string) {
  console.log(string.toLowerCase())
 } 
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
    return "I can't hear you!";
@@ -23,7 +23,6 @@ function sayHiToGrandma(string) {
   return "I love you, too.";
 }
 }
-=======
   console.log ().toUpperCase()
 }
 
@@ -31,4 +30,3 @@ function logWhisper(string) {
  console.log().toLowerCase()
 } 
   
->>>>>>> b99cca4eeb3b9641a2d0d92e3755905c87495fb1
